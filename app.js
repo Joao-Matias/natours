c4c7b28e8355530f4a49ffc831d84342150adcfb
Joao-Matias <jdmatias@live.com>
2024-02-25 15:45:00 +0000

    .

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+
+  next();
+});
+
+// 3)ROUTES
+
+// app.get(`/api/v1/tours`, getAllTours);
+// app.get(`/api/v1/tours/:id`, getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// ERROR HANDLING MIDDLEWARE - ALWAYS 4 ARGUMENTS AND ONLY 1 PER APP
+app.use(globalErrorHandler);
+
+module.exports = app;
